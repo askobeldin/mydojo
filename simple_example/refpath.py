58c71be437d670a8b1d5bebf25b87a61276cb9e2
askobeldin <askobeldin@gmail.com>
2016-09-13 17:20:12 +0300

    refpath changed

--- simple_example/refpath.py
+++ simple_example/refpath.py
@@ -7,7 +7,8 @@ import types
 
 pathways = types.SimpleNamespace()
 
-CURDIR = os.path.dirname(__file__)
+# CURDIR = os.path.dirname(__file__)
+pathways.curdir = os.path.abspath(os.path.dirname(__file__))
 
 def mkabspath(*chunks):
     items = list(chunks)
@@ -19,19 +20,24 @@ def mkstaticfilepath(root, path):
 
 def printinfo(ns):
     for k in vars(ns):
-        print('{:<10}{}'.format(k, getattr(ns, k)))
+        print('{:<12}{}'.format(k, getattr(ns, k)))
+
+def printinfo2(ns, lst):
+    for k in lst:
+        print('{:<12}{}'.format(k, getattr(ns, k)))
 
 
 # html folder
-pathways.html = mkabspath(CURDIR, 'html')
+pathways.html = mkabspath(pathways.curdir, 'html')
 # static folder
-pathways.static = mkabspath(CURDIR, 'static')
+pathways.static = mkabspath(pathways.curdir, 'static')
 # app folder
-pathways.app = mkabspath(CURDIR, 'app')
+pathways.app = mkabspath(pathways.curdir, 'app')
 
 
 # Dojo release directory
-pathways.dr = mkabspath('..', CURDIR, 'dojo-release-1.11.2')
+# pathways.dr = mkabspath('..', CURDIR, 'dojo-release-1.11.2')
+pathways.dr = mkabspath(pathways.curdir, '..', 'dojo-release-1.11.2')
 # dojo directory
 pathways.dojo = mkabspath(pathways.dr, 'dojo')
 # dijit directory
@@ -41,8 +47,12 @@ pathways.dojox = mkabspath(pathways.dr, 'dojox')
 # Dojo themes directory
 pathways.themes = mkabspath(pathways.dr, 'themes')
 
-printinfo(pathways)
+names = ('curdir',
+         'dr', 'dojo', 'dijit', 'dojox', 'themes',
+         'app', 'html', 'static')
 
-print('filepath for app')
-print(mkstaticfilepath(pathways.app, 'logger.js'))
+# printinfo(pathways)
+printinfo2(pathways, names)
 
+msg = '\nAsk file {0[0]!r} in {0[1]}.'
+print(msg.format(mkstaticfilepath(pathways.app, 'logger.js')))
